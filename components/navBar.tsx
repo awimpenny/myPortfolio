6ae6a4b844506533dac6f60fb559f89da9ebdfa2
Adam.Wimpenny <97692175+awimpenny@users.noreply.github.com>
2023-12-17 21:48:24 +0000

    upd; (nav colours)

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -41,7 +41,7 @@ const NavBar = () => {
                     className='flex items-center justify-center w-12 h-12 bg-gray-200 rounded-full dark:bg-[#1e293b] text-2xl general-ring-state '
                 >
                     <LuSun className='hidden dark:block' />
-                    <LuMoonStar className='dark:hidden' />
+                    <LuMoonStar className='dark:hidden text-black' />
                 </button>
             </div>
         </div>
@@ -58,10 +58,10 @@ const NavLink = (props: {
 
     return (
         <a
-            className="font-bold text-gray-900 dark:text-gray-200 'hidden md:inline-block p-1 sm:px-6 sm:py-2 rounded-full hover:text-teal-500 transition-all"
+            className="font-bold text-gray-900 dark:text-gray-200 'hidden md:inline-block p-1 sm:px-6 sm:py-2 rounded-full hover:text-blue-500 dark:hover:text-blue-500 transition-all"
             href={props.data.link}
         >
-            <span className={`py-1 border-b-2 border-transparent ${path === props.data.link && 'border-teal-500'} `}>
+            <span className={`py-1 border-b-2 border-transparent ${path === props.data.link && 'border-blue-500'} `}>
                 {props.data.label}
             </span>
         </a>
