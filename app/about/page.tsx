9598b2a21959280ed3f5e77ca957ff513ae6a37c
Adam.Wimpenny <97692175+awimpenny@users.noreply.github.com>
2023-12-18 23:15:37 +0000

    fix(about): &apos;

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,16 +6,16 @@ const About = () => {
             <h1 className='text-4xl text-center text-blue-500 mt-10 mb-2 tracking-wide'>About Me</h1>
             <h2 className='text-5xl text-center tracking-wide font-bold'>A little bit about me</h2>
             <p className='text-left my-10 text-lg tracking-wide'>
-                Hi, I'm Adam, a web developer passionate about crafting user-centric digital experiences. I specialize
-                in creating efficient and responsive websites that merge form with function. Let's create something
-                amazing together!
+                Hi, I&apos;m Adam, a web developer passionate about crafting user-centric digital experiences. I
+                specialize in creating efficient and responsive websites that merge form with function. Let&apos;s
+                create something amazing together!
             </p>
             <div className='rounded-md italic p-5 bg-blue-500/20 text-md tracking-wide'>
                 "The original idea of the web was that it should be a collaborative space where you can communicate
                 through sharing information. The idea was that by writing something together, and as people worked on
                 it, they could iron out misunderstanding, build common ground, and work towards consensus. The web is
-                read-write; it's not just people consuming. It's not just people producing. It's people connecting, and
-                we're still building it." - Tim Berners-Lee
+                read-write; it&apos;s not just people consuming. It&apos;s not just people producing. It&apos;s people
+                connecting, and we&apos;re still building it." - Tim Berners-Lee
             </div>
             <p className='text-left mt-10 text-lg tracking-wide'>
                 I thrive on collaboration and meticulous problem-solving, ensuring every project I work on delivers not
