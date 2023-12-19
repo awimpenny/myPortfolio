140b3f48a62b224c2f57a0128ada29652b73bc2e
Adam.Wimpenny <97692175+awimpenny@users.noreply.github.com>
2023-12-19 17:14:18 +0000

    style: changed layout of prjects and font sizes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,21 +3,21 @@
 const About = () => {
     return (
         <main>
-            <h1 className='text-4xl text-center text-blue-500 mt-10 mb-2 tracking-wide'>About Me</h1>
-            <h2 className='text-5xl text-center tracking-wide font-bold'>A little bit about me</h2>
-            <p className='text-left my-10 text-lg tracking-wide'>
+            <h1 className='text-lg md:text-xl text-center text-blue-500 mt-10 mb-2 tracking-wide'>ABOUT ME</h1>
+            <h2 className='text-3xl md:text-4xl text-center tracking-wide font-bold'>A little bit about me</h2>
+            <p className='text-left my-10 text-sm md:text-lg tracking-wide'>
                 Hi, I&apos;m Adam, a web developer passionate about crafting user-centric digital experiences. I
                 specialize in creating efficient and responsive websites that merge form with function. Let&apos;s
                 create something amazing together!
             </p>
-            <div className='rounded-md italic p-5 bg-blue-500/20 text-md tracking-wide'>
+            <div className='rounded-md italic p-5 bg-blue-500/20 text-sm md:text-md tracking-wide'>
                 &quot;The original idea of the web was that it should be a collaborative space where you can communicate
                 through sharing information. The idea was that by writing something together, and as people worked on
                 it, they could iron out misunderstanding, build common ground, and work towards consensus. The web is
                 read-write; it&apos;s not just people consuming. It&apos;s not just people producing. It&apos;s people
                 connecting, and we&apos;re still building it.&quot; - Tim Berners-Lee
             </div>
-            <p className='text-left mt-10 text-lg tracking-wide'>
+            <p className='text-left mt-10 text-sm md:text-lg tracking-wide'>
                 I thrive on collaboration and meticulous problem-solving, ensuring every project I work on delivers not
                 just visually stunning designs but also top-notch functionality.
             </p>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,10 +2,10 @@
 import Image from 'next/image';
 interface Iprojects {
     title: string;
-    desc: string;
-    img: string;
-    tags: string[];
-    link: string;
+    desc?: string;
+    img?: string;
+    tags?: string[];
+    link?: string;
     flipped: boolean;
 }
 
@@ -14,26 +14,36 @@ const Projects = () => {
         {
             title: 'Crystal Details',
             desc: 'Worked with the design and marketing team to developer a responsive web application built from ground up using WordPress PHP framework, with high focus on design suitable for all platforms and Search Engine Optimization, to achieve the best online presence and highest rankings.',
-            img: '/crystaldetails.jpg',
             tags: ['JavaScript', 'Html', 'CSS', 'UI/UX'],
             link: 'https://www.crystaldetails.co.uk/',
             flipped: false,
         },
         {
+            title: 'Crystal Details',
+            img: '/crystaldetails.jpg',
+            flipped: false,
+        },
+        {
             title: 'Evol-ve',
             desc: 'Worked with the design and marketing team to developer a responsive web application built from ground up using WordPress PHP framework, with high focus on design suitable for all platforms and Search Engine Optimization, to achieve the best online presence and highest rankings.',
-            img: '/evolve.jpg',
             tags: ['JavaScript', 'Html', 'CSS', 'UI/UX'],
-            link: 'https://www.evol-ve.com/',
+            link: 'https://evol-ve.com/',
+            flipped: true,
+        },
+        {
+            title: 'Evol-ve',
+            img: '/evolve.jpg',
             flipped: true,
         },
     ];
 
     return (
         <main>
-            <h1 className='text-4xl text-center text-blue-500 mt-10 mb-2 tracking-wide'>My Projects</h1>
-            <h2 className='text-5xl text-center tracking-wide font-bold mb-20'>Check out some of my work.</h2>
-            <div className='flex flex-col my-10 gap-20'>
+            <h1 className='text-lg md:text-xl text-center text-blue-500 mt-10 mb-2 tracking-wide'>MY PROJECTS</h1>
+            <h2 className='text-3xl md:text-4xl text-center tracking-wide font-bold mb-20'>
+                Check out some of my work.
+            </h2>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-x-12 gap-y-10 lg:gap-y-24 mb-10'>
                 {projects.map((v: Iprojects, i: number) => {
                     return <ProjectCard data={v} key={i} />;
                 })}
@@ -45,41 +55,48 @@ const Projects = () => {
 const ProjectCard = (props: { data: Iprojects }) => {
     const tagStyle = 'inline-block bg-gray-200 dark:bg-[#2f2b2b] text-sm leading-5 mr-2 mb-2 px-4 py-2 rounded-full';
 
-    return (
-        <div className='grid lg:grid-cols-3 gap-10'>
-            <div className={`${props.data.flipped && 'lg:order-3'} flex flex-col col-span-1`}>
-                <div className='text-3xl font-bold tracking-wide'>
-                    {props.data.title} <span className='text-base'>(Dec 2023)</span>
+    if (props.data.img) {
+        return (
+            <div className='md:col-span-2 m-auto'>
+                <div className='grid grid-cols-1 gap-4 lg:grid-cols-5'>
+                    <div className='relative col-span-5 overflow-hidden rounded-lg'>
+                        <Image
+                            src={props.data.img}
+                            alt={props.data.title}
+                            width={800}
+                            height={800}
+                            className={`${props.data.flipped && 'lg:order-1'} rounded-lg`}
+                        />
+                    </div>
                 </div>
-                <div className='text-lg mt-10 tracking-wide'>{props.data.desc}</div>
-                <p className='mt-5'>
-                    {props.data.tags.map((v: string, i: number) => {
-                        return (
-                            <span className={tagStyle} key={i}>
-                                {v}
-                            </span>
-                        );
-                    })}
-                </p>
-                <a href={props.data.link} target='_blank'>
-                    <button
-                        aria-label='Go to Crystal Details'
-                        type='button'
-                        className='mt-5 md:inline-flex py-3 px-2 md:px-12 rounded-md w-full bg-[#2f2b2b] dark:bg-white text-white dark:text-black items-center justify-center general-ring-state font-medium'
-                    >
-                        Visit {props.data.title}
-                    </button>
-                </a>
             </div>
-            <Image
-                src={props.data.img}
-                alt={props.data.title}
-                width={800}
-                height={800}
-                className={`${
-                    props.data.flipped && 'lg:order-1'
-                } object-cover bg-no-repeat rounded-lg w-full h-full col-span-2`}
-            />
+        );
+    }
+
+    return (
+        <div className={`${props.data.flipped && 'lg:order-3'} flex flex-col col-span-1`}>
+            <div className='text-3xl font-bold tracking-wide'>
+                {props.data.title} <span className='text-sm'>(Dec 2023)</span>
+            </div>
+            <div className='text-lg mt-10 tracking-wide'>{props.data.desc}</div>
+            <p className='mt-5'>
+                {props.data.tags?.map((v: string, i: number) => {
+                    return (
+                        <span className={tagStyle} key={i}>
+                            {v}
+                        </span>
+                    );
+                })}
+            </p>
+            <a href={props.data.link} target='_blank'>
+                <button
+                    aria-label='Go to Crystal Details'
+                    type='button'
+                    className='mt-5 md:inline-flex py-3 px-2 md:px-12 rounded-md w-full bg-[#2f2b2b] dark:bg-white text-white dark:text-black items-center justify-center general-ring-state font-medium'
+                >
+                    Visit {props.data.title}
+                </button>
+            </a>
         </div>
     );
 };
