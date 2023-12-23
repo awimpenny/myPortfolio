b3c18b79e5cf45677e1ccfd5dbd6928d4a7f2f1f
Adam.Wimpenny <97692175+awimpenny@users.noreply.github.com>
2023-12-23 15:01:37 +0000

    feat(projects): change grid logic

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 import ContactForm from '@/components/about/contact';
 interface Iprojects {
     title: string;
-    emoji?: string;
-    date?: string;
-    desc?: string;
-    img?: string;
-    tags?: string[];
-    link?: string;
+    emoji: string;
+    date: string;
+    desc: string;
+    img: string;
+    tags: string[];
+    link: string;
     flipped: boolean;
 }
 
@@ -22,11 +22,7 @@ const Projects = () => {
             tags: ['JavaScript', 'Html', 'CSS', 'UI/UX'],
             link: 'https://www.crystaldetails.co.uk/',
             flipped: false,
-        },
-        {
-            title: 'Crystal Details',
             img: '/crystaldetails.jpg',
-            flipped: false,
         },
         {
             title: 'Evol-ve',
@@ -36,11 +32,18 @@ const Projects = () => {
             tags: ['JavaScript', 'Html', 'CSS', 'UI/UX'],
             link: 'https://evol-ve.com/',
             flipped: true,
-        },
-        {
-            title: 'Evol-ve',
             img: '/evol-ve.jpg',
-            flipped: true,
+        },
+
+        {
+            title: 'Adamtecture',
+            emoji: '🏢',
+            date: 'Dec.2023',
+            desc: 'A project I made just for fun showing what I can design. This is just the home page as I wanted to show off my designing more than what I can code.',
+            tags: ['JavaScript', 'Html', 'CSS', 'UI/UX'],
+            link: 'https://adamtecture.vercel.app/',
+            flipped: false,
+            img: '/adamtecture.jpg',
         },
     ];
 
@@ -50,9 +53,17 @@ const Projects = () => {
             <h2 className='text-3xl md:text-4xl text-center tracking-wide font-bold mb-20'>
                 Check out some of my work.
             </h2>
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-x-12 gap-y-10 lg:gap-y-24 mb-10'>
+            <div className='flex flex-col w-full mb-10 gap-y-10 lg:gap-y-24'>
                 {projects.map((v: Iprojects, i: number) => {
-                    return <ProjectCard data={v} key={i} />;
+                    return (
+                        <div
+                            key={i}
+                            className='grid grid-cols-1 lg:grid-cols-3 gap-x-12 gap-y-10 lg:gap-y-24 w-full h-full'
+                        >
+                            <ProjectCard data={v} />
+                            <ProjectImg data={v} />
+                        </div>
+                    );
                 })}
             </div>
             <hr className="h-10 w-full text-center my-16 border-none before:text-2xl before:leading-8 before:text-black  dark:before:text-gray-300 before:content-['~_~_~']"></hr>
@@ -61,31 +72,31 @@ const Projects = () => {
     );
 };
 
+const ProjectImg = (props: { data: Iprojects }) => {
+    return (
+        <div className='md:col-span-2 m-auto'>
+            <div className='grid grid-cols-1 gap-4 lg:grid-cols-5'>
+                <div className='relative col-span-5 overflow-hidden rounded-lg'>
+                    <Image
+                        src={props.data.img}
+                        alt={props.data.title}
+                        width={800}
+                        height={800}
+                        className={`${
+                            props.data.flipped && 'lg:order-1'
+                        } rounded-lg border-gray-600/20 dark:border-gray-200/20 border-4`}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
 const ProjectCard = (props: { data: Iprojects }) => {
     const tagStyle = 'inline-block bg-gray-200 dark:bg-[#2f2b2b] text-sm leading-5 mr-2 mb-2 px-4 py-2 rounded-full';
 
-    if (props.data.img) {
-        return (
-            <div className='md:col-span-2 m-auto'>
-                <div className='grid grid-cols-1 gap-4 lg:grid-cols-5'>
-                    <div className='relative col-span-5 overflow-hidden rounded-lg'>
-                        <Image
-                            src={props.data.img}
-                            alt={props.data.title}
-                            width={800}
-                            height={800}
-                            className={`${
-                                props.data.flipped && 'lg:order-1'
-                            } rounded-lg border-gray-600/20 dark:border-gray-200/20 border-4`}
-                        />
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
     return (
-        <div className={`${props.data.flipped && 'lg:order-3'} flex flex-col col-span-1`}>
+        <div className={`${props.data.flipped && 'lg:order-3'} flex flex-col col-span-1 h-full justify-evenly`}>
             <div className='text-3xl font-bold tracking-wide whitespace-pre-line md:whitespace-nowrap'>
                 {props.data.emoji} {props.data.title} <span className='text-sm'>({props.data.date})</span>
             </div>
